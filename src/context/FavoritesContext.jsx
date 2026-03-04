4983eaf006988abaca90b14118b6f96bdd28c74b
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-04 05:04:31 -0500

    working on favoriterecipe

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,4 +1,25 @@
+import { createContext, useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const FavoritesContext = createContext();
+
 function FavoritesContext(){
+
+const[favorites, setfavorites] = useLocalStorage("favorites", []);
+
+const addFavorites =(id) =>{
+    if(!favorites.includes(id)){
+        setfavorites([...favorites, id]);
+    }
+};
+const removeFavarite = (id) => {
+    setfavorites(favorites.filter((fav) => fav !== id));
+}
+
+const isFavorites = (id) =>{
+    re
+}
+
     return(
         <div>FavoritesContext</div>
     );
